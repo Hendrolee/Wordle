e4da7043dcc452ee4b373375d44825b1f745cdf0
Hendro Lee <hendrolee39@gmail.com>
2022-06-26 21:17:20 +1000

    Fix reveal tile logic

--- index2.js
+++ index2.js
@@ -4,12 +4,13 @@ window.onload = () => {
   document.querySelector("#form1_0").focus();
 };
 
-const answerString = "neuer";
+const answerString = "henry";
 const answerArray = answerString.split("");
 let gameCount = 1; // to move input field in querySelector
 const submitButton = document.querySelector("#submit_button");
 let inputArray = [];
 const playersNameArray = [
+  "creek",
   "amavi",
   "bowen",
   "cisse",
@@ -20,7 +21,7 @@ const playersNameArray = [
   "henry",
   "iwobi",
   "jones",
-  "eante",
+  "kante",
   "lopez",
   "messi",
   "neuer",
@@ -44,10 +45,6 @@ const updateDisplayPanel = () => {
       .querySelector(`#form${gameCount}_${i}`)
       .setAttribute("value", inputArray[i] || "");
   }
-
-  // if (array1.length) {
-  //   document.querySelector(`#form${count}_${array1.length - 1}`).focus();
-  // }
 };
 
 const keyupHandler = (e) => {
@@ -74,21 +71,30 @@ const keyupHandler = (e) => {
 };
 
 const revealTile = (gameCount) => {
-  for (let i = 0; i < answerArray.length; i++) {
-    if (answerArray[i] === inputArray[i]) {
+  let tempAnswerArray = [...answerArray]; // ['h', 'e', 'n', 'r', 'y']
+
+  inputArray.forEach((letter, i) => {
+    // get the index of matched letter from tempAnswerArray
+    const tempAnswerIndex = tempAnswerArray.indexOf(letter);
+
+    if (letter === tempAnswerArray[i]) {
+      tempAnswerArray.splice(tempAnswerIndex, 1);
       document
         .querySelector(`#form${gameCount}_${i}`)
-        .setAttribute("style", "background-color: green");
-    } else if (answerArray.includes(inputArray[i])) {
+        .setAttribute("style", "background-color:green");
+    } else if (tempAnswerArray.includes(letter)) {
+      tempAnswerArray.splice(tempAnswerIndex, 1);
       document
         .querySelector(`#form${gameCount}_${i}`)
-        .setAttribute("style", "background-color: orange");
+        .setAttribute("style", "background-color:orange");
     } else {
       document
         .querySelector(`#form${gameCount}_${i}`)
-        .setAttribute("style", "background-color: gray");
+        .setAttribute("style", "background-color:grey");
     }
-  }
+  });
+  // reseting the looped array
+  tempAnswerArray = [...answerArray];
 };
 
 const submitButtonHandler = () => {
@@ -129,104 +135,13 @@ document.querySelectorAll("input").forEach((input) => {
 // previous form is inaccessible after valid input is submited //
 // --- alternative ---
 // all other forms are readOnly except current form //
-// Add Enter event for submission
+// Add Enter event for submission //
 
-// display different background for any word that is within the answer
-// adapt phone screen size
-
+// display different background for any word that is within the answer //
 // why there is no selector when all inputs are readOnly ?
 // Because letter is rendered when key is pressed (setAttribute/removeAttribute)
 
-// filter the matching letter against answer => i.e. [e,n]
-// const matchedLetter = inputArray.filter((letter) =>
-// answerArray.includes(letter)
-// );
-// console.log("matchedLetter:", matchedLetter);
-
-// // find the position of matchedLetter
-// let matchLetterPosition = [];
-// matchedLetter.map((letter) => {
-// const position = inputArray.indexOf(letter);
-// matchLetterPosition.push(position);
-// });
-// console.log("matchedLetter position:", matchLetterPosition);
-
-// // positions of answerArray
-// const answerArrayPosition = answerArray.map((letter) =>
-// answerArray.indexOf(letter)
-// );
-// console.log(answerArray);
-
-// compare matchLetterPosition with answerArray position & change the color of the matchedLetter
-
-// Fix :
-// duplicate alphabets in single alphabet answer
-
-// const revealTile = (gameCount, answer) => {
-//   // ['h', 'e', 'n', 'r', 'y']
-//   const answerArray = answer.split("");
-
-//   // filter the matching letter against answer => i.e. [e,n]
-//   const matchedLetter = inputArray.filter((letter) =>
-//     answerArray.includes(letter)
-//   );
-//   console.log("matchedLetter:", matchedLetter);
-
-//   let matchLetterPosition = matchedLetter.map((letter) =>
-//     inputArray.indexOf(letter)
-//   );
-//   console.log(matchLetterPosition);
-
-//   for (let i = 0; i < answerArray.length; i++) {
-//     if (answerArray[i] === inputArray[i]) {
-//       console.log("green", answerArray[i]);
-//       document
-//         .querySelector(`#form${gameCount}_${i}`)
-//         .setAttribute("style", "background-color:green");
-//     } else if (answerArray.includes(inputArray[i])) {
-//       console.log("yellow", inputArray[i]);
-//       document
-//         .querySelector(`#form${gameCount}_${i}`)
-//         .setAttribute("style", "background-color:orange");
-//     } else {
-//       console.log("black", inputArray[i]);
-//       document
-//         .querySelector(`#form${gameCount}_${i}`)
-//         .setAttribute("style", "background-color: gray");
-//     }
-//   }
-
-//   Array.from(document.querySelectorAll("#form1 input")).map((input) => {
-//     // let index = answerArray.find((element) => element === input.value);
-//     // let value = answerArray.includes(input.value);
-//     // if (index) {
-//     //   input.setAttribute("style", "background-color:green");
-//     // } else if (value) {
-//     //   input.setAttribute("style", "background-color:orange");
-//     // } else {
-//     //   input.setAttribute("style", `background-color: ""`);
-//     // }
-//     // value ? input.setAttribute("style", "background-color:orange") : "";
-//   });
-// };
-
-// for (let i = 0; i < answer.length; i++) {
-//   if (answerSorted[i].letter === inputSorted[i].letter) {
-//     console.log("green");
-//     document
-//       .querySelector(`#form${gameCount}_${i}`)
-//       .setAttribute("style", "background-color: green");
-//   }
-// else if (
-
-// ) {
-//   console.log("yellow");
-//   document
-//     .querySelector(`#form${gameCount}_${i}`)
-//     .setAttribute("style", "background-color: orange");
-// }
-// }
-
 // fix :
-// when duplicate letter comes before green letter
-// when there are multiple duplicates
+// when duplicate letter comes before green letter //
+// when there are multiple duplicates //
+// adapt phone screen size
